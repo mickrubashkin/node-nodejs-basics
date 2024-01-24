63afb640400fb83bf8874ec912dd2440150cf2ff
MickRubashkin <mikhail.rubashkin@gmail.com>
2024-01-24 11:02:20 +0100

    add solution to hash section

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const readerStream = createReadStream(filePath);
+  readerStream.setEncoding('UTF8');
+
+  readerStream.on('data', (data) => {
+    const hash = createHash('sha256').update(data).digest('hex');
+    console.log(hash);
+  });
 };
 
 await calculateHash();
