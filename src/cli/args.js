f0ec63566681bcb51872874696bdc25e04b2e71e
MickRubashkin <mikhail.rubashkin@gmail.com>
2024-01-24 10:01:17 +0100

    add solution to cli section

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+  let result = [];
+
+  for (let i = 0; i < args.length; i += 1) {
+    if (args[i].substring(0, 2) === '--') {
+      result.push(`${args[i].substring(2)} is ${args[i + 1]}`);
+    }
+  }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const res = Object.entries(process.env)
+    .filter(([key, val]) => key.substring(0, 3) === 'RSS')
+    .map(([key, val]) => `${key}=${val}`)
+    .join('; ');
+
+  console.log(res);
 };
 
 parseEnv();
